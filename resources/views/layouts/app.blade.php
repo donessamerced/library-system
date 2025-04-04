5b3e04f243090015bc51076b44256bae093e0bb2
donessamerced <donessamercedcastillon@gmail.com>
2025-04-04 14:25:50 +0800

    second commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>SLSU-BH MANAGEMENT SYSTEM</title>
+    <title>Library System</title>
     <link rel="icon" type="image/x-icon" href="../assets/img/favicon/NSB.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
